866c119ff5e0ecaa438779c47557c1aeed007417
Dhrubo111 <irfansadidhrubo@gmail.com>
2023-11-16 01:39:06 +0600

    first working version for pc

--- app.js
+++ app.js
@@ -1,113 +1,113 @@
-let bord = document.querySelector(".bord")
+let bord = document.querySelector(".bord");
+let slider = document.getElementById("mySlider");
+let rangeValue = document.getElementById("rangeValue");
+let resetBtn = document.querySelector(".reset");
+let rainbowBtn = document.querySelector(".ranbow");
+let eraserBtn = document.querySelector(".eraser");
+let massage = document.querySelector("h2");
 
-let slider = document.getElementById("mySlider")
+let colors = document.querySelector(".color-pick");
+let colorPicker = document.querySelector("#colorPicker"); // Added color picker input
+let allcells;
 
-let rangeValue = document.getElementById("rangeValue")
+const predefinedColors = [
+  '#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF', '#00FFFF'
+];
 
+let currentMode = "black"; // Added variable to track current mode
 
-let allRows = document.querySelectorAll(".row")
-
-
-let resetBtn = document.querySelector(".reset")
-
-
-let allcells = document.querySelectorAll(".cell")
-
-
-
-
-//drawing the sliderValue
-
-function draw(sliderValue){
-
-    bord.innerHTML = ""
-
-   for(let i = 0 ; i < sliderValue; i++){
-
-    let row = document.createElement("div")
-
-    row.classList.add("row")
-
-    for(let j = 0 ; j < sliderValue ; j++){
-
-        let cell = document.createElement("div")
-        cell.classList.add("cell")
-        row.appendChild(cell)
-    }
-    bord.appendChild(row)
-
-   }
-   allcells = document.querySelectorAll(".cell");
+function changeColor() {
+  return predefinedColors[Math.floor(Math.random() * predefinedColors.length)];
 }
 
-draw(slider.value)
-
-
-slider.addEventListener(
-    "input" , ()=>{
-
-        // work this when the slider is moved 
-
-        let changedValue = slider.value
-
-        draw(changedValue)
+function draw(sliderValue) {
+  bord.innerHTML = "";
+  for (let i = 0; i < sliderValue; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < sliderValue; j++) {
+      let cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
     }
-)
+    bord.appendChild(row);
+  }
+  allcells = document.querySelectorAll(".cell");
+}
 
+draw(slider.value);
 
+slider.addEventListener("input", () => {
+  let changedValue = slider.value;
+  draw(changedValue);
+  rangeValue.textContent = changedValue;
+});
 
-// default mouse move  function
-// Function to handle cell interactions
-function handleCellInteraction(element) {
-    if (element.classList.contains("cell")) {
+function handleCellInteraction(element, color) {
+  if (element.classList.contains("cell")) {
+    element.style.backgroundColor = color;
+  }
+}
+
+function handleCellInteractionRandom(element) {
+  if (element.classList.contains("cell")) {
+    if (currentMode === "black") {
       element.style.backgroundColor = "black";
+    } else if (currentMode === "random") {
+      element.style.backgroundColor = changeColor();
+    } else if (currentMode === "eraser") {
+      element.style.backgroundColor = "lightgray";
+    } else if (currentMode === "color-picker") {
+      // Use the selected color from the color picker
+      element.style.backgroundColor = colorPicker.value;
     }
   }
-  
-  // Use event delegation for mousemove and mouseleave events
-  bord.addEventListener("mousemove", (event) => {
-    handleCellInteraction(event.target);
-  });
-  
-  bord.addEventListener("touchmove", (event) => {
-    // Prevent default to avoid unwanted scrolling on touch devices
-    event.preventDefault();
-    handleCellInteraction(event.target);
-  });
-  
-  bord.addEventListener("mouseleave", (event) => {
-    handleCellInteraction(event.target);
-  });
-  
-  bord.addEventListener("touchend", (event) => {
-    handleCellInteraction(event.target);
-  });
-  
-  // reset function
-  resetBtn.addEventListener("click", () => {
-    let defaultValue = (slider.value = 16);
-    rangeValue.textContent = defaultValue;
-    draw(defaultValue);
-  });
-  
+}
 
-// last 
+rainbowBtn.addEventListener("click", () => {
+  // If eraser mode or color-picker mode is active, set the mode to "black"
+  if (currentMode === "eraser" || currentMode === "color-picker") {
+    currentMode = "black";
+  } else {
+    // Toggle between "black" and "random" modes
+    currentMode = currentMode === "black" ? "random" : "black";
+  }
+});
 
+eraserBtn.addEventListener("click", () => {
+  // Set the mode to "eraser"
+  currentMode = "eraser";
+  massage.style.display = "inline";
+});
 
-//reset function 
+colors.addEventListener("click", () => {
+  // Set the mode to "color-picker"
+  currentMode = "color-picker";
+  massage.style.display = "inline";
+});
 
-resetBtn.addEventListener(
-    "click" , ()=>{
-       let defaultVlue =  slider.value = 16
-       rangeValue.textContent = defaultVlue
-        draw(defaultVlue)
+bord.addEventListener("mousemove", (event) => {
+  handleCellInteractionRandom(event.target);
+});
 
-        
-    }
-)
+bord.addEventListener("touchmove", (event) => {
+  event.preventDefault();
+  handleCellInteractionRandom(event.target);
+});
 
+bord.addEventListener("mouseleave", (event) => {
+  handleCellInteractionRandom(event.target);
+});
 
+bord.addEventListener("touchend", (event) => {
+  handleCellInteractionRandom(event.target);
+});
 
+resetBtn.addEventListener("click", () => {
+  let defaultValue = (slider.value = 16);
+  rangeValue.textContent = defaultValue;
+  draw(defaultValue);
 
-
-
+  // Reset the mode to "black" when resetting the board
+  currentMode = "black";
+});
